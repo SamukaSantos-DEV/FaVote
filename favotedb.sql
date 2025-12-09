4fed01a2bda3a07e04bc9f896523ebeecb4e041a
SamukaSantos-DEV <samuelsantos7796@gmail.com>
2025-12-09 17:31:46 -0300

    Update favotedb.sql

--- favotedb.sql
+++ favotedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06/12/2025 às 20:14
+-- Tempo de geração: 09/12/2025 às 21:31
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
